399f2b337c1fa5ee57ddd386a817e2e36ea2afef
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-04-17 08:38:16 +0200

    add category

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -12,6 +12,23 @@ export default{
       <img :src="project.image || 'https://picsum.photos/600/400'" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">{{ project.title }}</h5>
+        <div>
+          <span class="me-2 "><strong>Category:</strong></span>
+          <span
+            class="badge"
+            :style="'background-color:' + project.category.color">
+            {{ project.category.label }}
+          </span>
+        </div>
+        <div>
+          <span class="me-2"><strong>Technologies:</strong></span>
+          <span
+            v-for="tech in project.technologies"
+            class="badge m-2"
+            :style="'background-color:' + tech.color"
+            >{{ tech.label }}
+          </span>
+        </div>
         <p class="card-text">{{ project.description }}</p>
         <div class="d-flex gap-3">
           <a :href="project.project_link" class="btn btn-primary">Guarda la repository Git</a>
--- src/pages/DetailPage.vue
+++ src/pages/DetailPage.vue
@@ -15,7 +15,7 @@ export default {
       const projectId = this.$route.params.id;
       axios.get(store.api.baseUrl + `projects/${projectId}`).then((response) => {
         this.project = response.data;
-        
+
       });
     },
   },
@@ -27,7 +27,6 @@ export default {
 </script>
 
 <template>
-  
   <div class="text-center mt-5">
     <h1 class="mb-5">{{ project.title }}</h1>
       <span><strong class="m-2">Author: {{ project.author }}</strong></span>
@@ -38,6 +37,14 @@ export default {
             class="badge me-2"
             :style="'background-color:' + tech.color"
             >{{ tech.label }}
+          </span>
+      </p>
+      <p>
+          <strong class="me-2">Categoria: </strong>
+          <span
+            class="badge"
+            :style="'background-color:' + project.category.color">
+            {{ project.category.label }}
           </span>
       </p>
       <div><a :href="project.project_link" class="btn btn-primary">Guarda la repository Git</a></div>
